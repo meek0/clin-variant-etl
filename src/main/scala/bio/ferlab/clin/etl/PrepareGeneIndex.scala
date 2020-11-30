4f25705915ca04d4a8d89bdfd2454ca0c57a512d
jecos <jcostanza@ferlab.bio>
2020-11-30 14:25:55 -0500

    fix: #545 Index genes

---
+++ src/main/scala/bio/ferlab/clin/etl/PrepareGeneIndex.scala
@@ -0,0 +1,24 @@
+package bio.ferlab.clin.etl
+
+import org.apache.spark.sql.SparkSession
+
+object PrepareGeneIndex extends App {
+
+  val Array(output) = args
+
+  implicit val spark: SparkSession = SparkSession.builder
+    .enableHiveSupport()
+    .appName(s"Prepare Gene Index").getOrCreate()
+
+  run(output)
+
+  def run(output: String)(implicit spark: SparkSession): Unit = {
+    spark.sql("use clin")
+    spark.table("genes").select(
+      "chromosome", "symbol", "entrez_gene_id", "omim_gene_id", "hgnc", "ensembl_gene_id", "location", "name", "alias", "biotype")
+      .write.mode("overwrite")
+      .json(s"$output")
+  }
+
+}
+
