0189f96c3a4488469a053cac79f1d71833909913
botekchristophe <botekchristophe@gmail.com>
2021-02-25 11:47:50 -0500

    feat: occurrences etl now has unit tests

---
+++ src/test/scala/bio/ferlab/clin/etl/OccurrencesSpec.scala
@@ -0,0 +1,36 @@
+package bio.ferlab.clin.etl
+
+import bio.ferlab.clin.model.{BiospecimenOutput, ClinvarOutput, OccurrenceOutput, PatientOutput, VCFInput, VariantOutput}
+import bio.ferlab.clin.testutils.WithSparkSession
+import org.apache.spark.sql.SaveMode
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import java.sql.Date
+import java.time.LocalDate
+
+class OccurrencesSpec extends AnyFlatSpec with WithSparkSession with Matchers {
+
+  import spark.implicits._
+
+  spark.sql("CREATE DATABASE IF NOT EXISTS clin")
+  spark.sql("USE clin")
+
+  Seq(PatientOutput()).toDF.write.format("parquet").mode(SaveMode.Overwrite)
+    .option("path", "spark-warehouse/clin.db/patients")
+    .saveAsTable("clin.patients")
+
+  Seq(BiospecimenOutput()).toDF.write.format("parquet").mode(SaveMode.Overwrite)
+    .option("path", "spark-warehouse/clin.db/biospecimens")
+    .saveAsTable("clin.biospecimens")
+
+
+  "occurrences job" should "transform data in expected format" in {
+
+    val df = Seq(VCFInput()).toDF()
+
+    Occurrences.build(df, "BAT1").as[OccurrenceOutput].collect() should contain allElementsOf Seq(
+      OccurrenceOutput(`last_update` = Date.valueOf(LocalDate.now()))
+    )
+  }
+}
