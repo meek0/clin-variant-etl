6e9be2f89bb48bbcf45829bea489c140173c11f5
cbotek <cbotek@ferlab.bio>
2022-04-07 14:45:19 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 # deploys assembly jar to qa environment
 sbt assembly
 aws s3 --profile cqgc --no-verify-ssl --endpoint-url https://minio-cqgc.infojutras.com cp target/scala-2.12/clin-variant-etl.jar s3://cqgc-qa-app-datalake/jobs/jars/clin-variant-etl.jar
+aws s3 --profile cqgc --no-verify-ssl --endpoint-url https://minio-cqgc.infojutras.com cp src/main/resources/templates s3://cqgc-qa-app-datalake/jobs/templates/ --recursive
