039c201465bf674b959f69a5ef979383fcc7571e
Yann MARCOU <creativeyann17@gmail.com>
2023-01-25 15:28:33 -0500

    feat: CLIN-1695 - add deploy S3 script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
-# for testing purpose only
-# deploys assembly jar to qa environment
-sbt assembly
-aws s3 --profile cqgc --no-verify-ssl --endpoint-url https://minio-cqgc.infojutras.com cp target/scala-2.12/clin-variant-etl.jar s3://cqgc-qa-app-datalake/jobs/jars/clin-variant-etl.jar
-aws s3 --profile cqgc --no-verify-ssl --endpoint-url https://minio-cqgc.infojutras.com cp src/main/resources/templates s3://cqgc-qa-app-datalake/jobs/templates/ --recursive
+
+VERSION="${1}"
+ENV="${2:-qa}"
+USAGE="<version> <env=[qa|staging|prod](default=qa)>"
+
+showUsage()
+{
+  echo "Usage:"
+  echo "  sh ./deploy.sh $USAGE"
+  echo "  ./deploy.sh $USAGE (required: chmod +x ./launch.sh)"
+  echo
+}
+
+if [ -z "$VERSION" ]
+  then
+    showUsage
+    exit 1
+fi
+
+# download the JAR based on version tag, ex: v2.5.4
+echo "=== DOWNLOAD FROM GITHUB ==="
+wget -O clin-variant-etl.jar https://github.com/Ferlab-Ste-Justine/clin-variant-etl/releases/download/$VERSION/clin-variant-etl.jar
+
+# copy + list S3, update profile name if necessary
+echo "=== COPY TO S3 ($ENV) ==="
+aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 cp clin-variant-etl.jar s3://cqgc-$ENV-app-datalake/jars/clin-variant-etl-$VERSION.jar
+
+echo "=== JARS IN S3 ($ENV) ==="
+aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 ls s3://cqgc-$ENV-app-datalake/jars --recursive
