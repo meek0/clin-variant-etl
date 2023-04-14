cbafbd619e6ca36ac8f9fad0ffae4eb3ac20e801
Yann MARCOU <creativeyann17@gmail.com>
2023-04-14 10:45:59 -0400

    feat: CLIN-1884 - save ETL jar with version

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,11 @@ fi
 
 # download the JAR based on version tag, ex: v2.5.4
 echo "=== DOWNLOAD FROM GITHUB ==="
-wget -O clin-variant-etl.jar https://github.com/Ferlab-Ste-Justine/clin-variant-etl/releases/download/$VERSION/clin-variant-etl.jar
+wget -O clin-variant-etl-$VERSION.jar https://github.com/Ferlab-Ste-Justine/clin-variant-etl/releases/download/$VERSION/clin-variant-etl.jar
 
 # copy + list S3, update profile name if necessary
 echo "=== COPY TO S3 ($ENV) ==="
-aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 cp clin-variant-etl.jar s3://cqgc-$ENV-app-datalake/jars/clin-variant-etl-$VERSION.jar
+aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 cp clin-variant-etl-$VERSION.jar s3://cqgc-$ENV-app-datalake/jars/clin-variant-etl-$VERSION.jar
 
 echo "=== JARS IN S3 ($ENV) ==="
 aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 ls s3://cqgc-$ENV-app-datalake/jars --recursive
