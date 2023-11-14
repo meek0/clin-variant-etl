f18173edaf572ca6003ed0fb9c1bfefdd26f156f
Karine St-Onge <kstonge@ferlab.bio>
2023-11-14 13:19:57 -0500

    feat: CLIN-2336 Adjust datatests

--- src/main/scala/bio/ferlab/clin/etl/qc/dictionary/package.scala
+++ src/main/scala/bio/ferlab/clin/etl/qc/dictionary/package.scala
@@ -362,47 +362,6 @@ package object dictionary {
     "null",
   )
 
-  val DicVarsomeAcmgClassificationName = Seq(
-    "PVS1",
-    "PS1",
-    "PS2",
-    "PS3",
-    "PS4",
-    "PM1",
-    "PM2",
-    "PM3",
-    "PM4",
-    "PM5",
-    "PM6",
-    "PP1",
-    "PP2",
-    "PP3",
-    "PP4",
-    "PP5",
-    "BA1",
-    "BS1",
-    "BS2",
-    "BS3",
-    "BS4",
-    "BP1",
-    "BP2",
-    "BP3",
-    "BP4",
-    "BP5",
-    "BP6",
-    "BP7",
-    "null",
-  )
-
-  val DicVarsomeAcmgVerdict = Seq(
-    "Pathogenic",
-    "Likely Pathogenic",
-    "Uncertain Significance",
-    "Likely Benign",
-    "Benign",
-    "null",
-  )
-
   val DicVepImpact = Seq(
     "HIGH",
     "MODERATE",
