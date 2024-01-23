d88bb8498bfd65dea474173fae3c11519acc7d1d
Yann MARCOU <creativeyann17@gmail.com>
2024-01-23 11:23:26 -0500

    feat: CLIN-2514 rename somatic VCFs

--- src/main/scala/bio/ferlab/clin/etl/conf/EtlConfiguration.scala
+++ src/main/scala/bio/ferlab/clin/etl/conf/EtlConfiguration.scala
@@ -55,7 +55,7 @@ object EtlConfiguration extends App {
     List(
       //s3://clin-{env}-app-files-import/201106_A00516_0169_AHFM3HDSXY/201106_A00516_0169_AHFM3HDSXY.hard-filtered.formatted.norm.VEP.vcf.gz
       DatasetConf("raw_snv"                        , clin_import  , "/{{BATCH_ID}}/*.hard-filtered.formatted.norm.VEP.vcf.gz"           , VCF    , OverWrite),
-      DatasetConf("raw_snv_somatic_tumor_only"     , clin_import  , "/{{BATCH_ID}}/*.dragen.WES_somatic-tumor_only.hard-filtered.vcf.gz", VCF    , OverWrite),
+      DatasetConf("raw_snv_somatic_tumor_only"     , clin_import  , "/{{BATCH_ID}}/*.dragen.WES_somatic-tumor_only.hard-filtered.norm.VEP.vcf.gz", VCF    , OverWrite),
       DatasetConf("raw_cnv"                        , clin_import  , "/{{BATCH_ID}}/*[0-9].cnv.vcf.gz"                                   , VCF    , OverWrite),
       DatasetConf("raw_cnv_somatic_tumor_only"     , clin_import  , "/{{BATCH_ID}}/*.dragen.WES_somatic-tumor_only.cnv.vcf.gz"          , VCF    , OverWrite),
       DatasetConf("raw_exomiser"                   , clin_import,   "/{{BATCH_ID}}/*.exomiser.variants.tsv"                           , CSV    , OverWrite, readoptions = tsv_with_headers),
