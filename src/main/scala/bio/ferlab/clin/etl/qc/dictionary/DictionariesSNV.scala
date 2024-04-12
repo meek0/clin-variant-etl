ca5ae456e80c9ff0f9774501b594ad3bf257f45c
Karine St-Onge <kstonge@ferlab.bio>
2024-04-12 11:20:10 -0400

    feat: CLIN-2500 Add datatests

--- src/main/scala/bio/ferlab/clin/etl/qc/dictionary/DictionariesSNV.scala
+++ src/main/scala/bio/ferlab/clin/etl/qc/dictionary/DictionariesSNV.scala
@@ -17,7 +17,10 @@ object DictionariesSNV extends TestingApp {
       shouldValuesContainedInDictionary(variant_centric.select(explode($"variant_external_reference")), DicVariantExternalReference: _*)("variant_external_reference"),
       shouldValuesContainedInDictionary(variant_centric.select($"cmc.tier"), DicCmcTier: _*)("cmc.tier"),
       shouldValuesContainedInDictionary(variant_centric.select($"hotspot"), DicHotspot: _*)("hotspot"),
-      shouldValuesContainedInDictionary(variant_centric.select(explode($"genes")).select(explode($"col.omim.inheritance_code")).select(explode($"col")), DicOMIM: _*)("genes.omim.inheritance_code")
+      shouldValuesContainedInDictionary(variant_centric.select(explode($"genes")).select(explode($"col.omim.inheritance_code")).select(explode($"col")), DicOMIM: _*)("genes.omim.inheritance_code"),
+      shouldValuesContainedInDictionary(variant_centric.select($"franklin_max.acmg_classification"), DicFraACMGClassMax: _*)("franklin_max.acmg_classification"),
+      shouldValuesContainedInDictionary(variant_centric.select(explode($"exomiser_max.acmg_evidence")), DicExoACMGEviMax: _*)("exomiser_max.acmg_evidence"),
+      shouldValuesContainedInDictionary(variant_centric.select(explode($"franklin_max.acmg_evidence")), DicFraACMGEviMax: _*)("franklin_max.acmg_evidence")
     )
   }
 }
