29bf67aa056ec804bd5f6f48f1c042ff6a4ac423
Laura Bégin <laurabegin1@gmail.com>
2025-09-17 10:37:09 -0400

    fix: CLIN-5155 Svclustering destination ids (#511)

--- src/main/scala/bio/ferlab/clin/etl/nextflow/RunNextflow.scala
+++ src/main/scala/bio/ferlab/clin/etl/nextflow/RunNextflow.scala
@@ -9,28 +9,28 @@ object RunNextflow {
   @main
   def normalize_svclustering_germline_del(rc: RuntimeETLContext): Unit = {
     val sourceId = "nextflow_svclustering_germline_del_output"
-    val destinationId = "normalized_svclustering_germline_del"
+    val destinationId = "nextflow_svclustering_germline_del"
     NormalizeSVClusteringGermline.run(rc, sourceId, destinationId)
   }
 
   @main
   def normalize_svclustering_germline_dup(rc: RuntimeETLContext): Unit = {
     val sourceId = "nextflow_svclustering_germline_dup_output"
-    val destinationId = "normalized_svclustering_germline_dup"
+    val destinationId = "nextflow_svclustering_germline_dup"
     NormalizeSVClusteringGermline.run(rc, sourceId, destinationId)
   }
 
   @main
   def normalize_svclustering_somatic_del(rc: RuntimeETLContext): Unit = {
     val sourceId = "nextflow_svclustering_somatic_del_output"
-    val destinationId = "normalized_svclustering_somatic_del"
+    val destinationId = "nextflow_svclustering_somatic_del"
     NormalizeSVClusteringSomatic.run(rc, sourceId, destinationId)
   }
 
   @main
   def normalize_svclustering_somatic_dup(rc: RuntimeETLContext): Unit = {
     val sourceId = "nextflow_svclustering_somatic_dup_output"
-    val destinationId = "normalized_svclustering_somatic_dup"
+    val destinationId = "nextflow_svclustering_somatic_dup"
     NormalizeSVClusteringSomatic.run(rc, sourceId, destinationId)
   }
 
